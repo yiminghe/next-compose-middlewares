9b9ccce5923c56fbf0a26da9a264d1a6d726ef10
yiminghe <yiminghe@gmail.com>
2024-05-31 17:50:22 +0800

    bundle analysis

--- packages/demo/next.config.js
+++ packages/demo/next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+   
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {}
+   
+  module.exports = withBundleAnalyzer(nextConfig)
