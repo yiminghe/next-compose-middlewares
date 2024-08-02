f56534e760c59693f9196ebbf4784e45e1c0dd91
yiminghe <yiminghe@gmail.com>
2024-08-03 02:12:41 +0800

    build

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 function r(...p) {
   return path.join(__dirname, ...p);
@@ -18,5 +19,23 @@ const start = readme.indexOf(
 const end = readme.indexOf('---------');
 
 readme = readme.slice(0, start) + md + readme.slice(end);
-
+const pkg = require('../package.json');
 fs.writeFileSync(r('../README.md'), readme);
+fs.writeFileSync(
+  r('../dist/package.json'),
+  JSON.stringify(
+    {
+      name: pkg.name,
+      version: pkg.version,
+      repository: pkg.repository,
+      dependencies: pkg.dependencies,
+    },
+    null,
+    2,
+  ),
+);
+
+fs.copyFileSync(r('../README.md'), r('../dist', 'README.md'));
+
+execSync('rm -rf ' + r('../dist/dist-types'), { stdio: 'inherit' });
+execSync('rm -rf ' + r('../dist/tsdoc-metadata.json'), { stdio: 'inherit' });
